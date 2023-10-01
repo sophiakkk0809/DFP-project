d34b7a3bba0b6b4a7b357025285432efb88d9eb9
alexhewitson <97577948+alexhewitson@users.noreply.github.com>
2023-10-01 11:28:17 -0400

    Add files via upload

---
+++ TJ_Scraping.py
@@ -0,0 +1,152 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import *
+import csv
+
+"""
+This Python file scrapes product information from the food section from the Trader Joe's website. It outputs
+the information in a .csv file called output.csv Functions beginning with '_' are private.
+
+Alex Hewitson
+"""
+
+# Initialize an empty dictionary to store the product information
+_prod_dict = {}
+
+
+def _scrape_all_products(driver):
+    """
+    Logic for clicking the next page button and calling the scrape_page function when appropriate
+    """
+
+    next_button_selector = ".Pagination_pagination__arrow__3TJf0.Pagination_pagination__arrow_side_right__9YUGr"
+
+    # loop through each page in the food category
+    while True:
+        try:
+            # scroll to the next page button
+            driver.execute_script("window.scrollTo(0, 3500);")
+            
+            # Wait for the next page button to be clickable
+            next_button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, next_button_selector))
+            )
+
+            # scrape data from the current page
+            _scrape_page(driver)
+
+            # Use ActionChains to click the next button with a slight delay
+            action = ActionChains(driver)
+            action.move_to_element(next_button).pause(1).click().perform()
+
+        except TimeoutException:
+            # scrape the last page
+            _scrape_page(driver)
+            break
+
+        except Exception as e:
+            # handle errors
+            print(f"Error: {e}")
+            break
+
+
+def _scrape_page(driver):
+    """
+    Puts the products in the TJs page into a dictionary in the format of product name: (category, price, quantity)
+    """
+
+    # get the list of products from the HTML
+    prods_list_str = driver.find_element(By.CLASS_NAME, "ProductList_productList__list__3-dGs").text
+
+    # split individual products into a list
+    product_entries = prods_list_str.split("ADD TO LIST\n")
+
+    # format each product's information and add to the dictionary
+    for prod in product_entries:
+        prod_details = prod.strip().split('\n')
+        # dictionary format = product name: (category, price, quantity)
+        _prod_dict[prod_details[1]] = (prod_details[0], prod_details[2].split('/')[0], prod_details[2].split('/')[1])
+
+
+def scrape_data():
+    """
+    Orchestrates the process of scraping data from the Trader Joes website and returns the data in _prod_dict
+    """
+
+    _prod_dict.clear()
+    # start the webpage
+    options = Options()
+    options.page_load_strategy = 'normal'
+    options.add_argument('--headless=new')
+
+    driver = webdriver.Chrome(options=options)
+
+    driver.set_window_size(880, 1080)
+    driver.get("https://www.traderjoes.com/home/products/category/food-8")
+
+    print("TJs Scraping in progress. This will take a few minutes...")
+    # scrape the webpage
+    _scrape_all_products(driver)
+    driver.quit()
+
+    print("Finished Scraping")
+
+    return _prod_dict
+
+
+def load_data():
+    """
+    Accepts a .csv file of preloaded product data (outputted from _write_file()) and converts it to a dictionary in _prod_dict
+    """
+    
+    _prod_dict.clear()
+
+    with open('preloaded_tjs.csv', mode='r', newline='') as file_input:
+        reader = csv.DictReader(file_input)
+        
+        for row in reader:
+            # Create a tuple containing category, price, and quantity
+            product_data = (row['Category'], row['Price'], row['Quantity'])
+            
+            # Add the data to the dictionary with product_name as the key
+            _prod_dict[row['Product Name']] = product_data
+    
+    return _prod_dict
+
+
+def _write_file():
+    """
+    Expects the dictionary format to be product name: (category, price, quantity) then converts the dictionary to a .csv
+    """
+
+    # Open the CSV file for writing
+    with open('output.csv', 'w', newline='') as file_output:
+    # Create a CSV writer object
+        csv_writer = csv.writer(file_output)
+        
+        # Write the header row
+        csv_writer.writerow(['Product Name', 'Category', 'Price', 'Quantity'])
+        
+        # Write the data rows
+        for product_name, product_info in _prod_dict.items():
+            csv_writer.writerow([product_name, *product_info])
+
+
+def main():
+    # scrape new product data from the website
+    scrape_data()
+
+    # load existing product data from preloaded_tjs.csv
+    #load_data()
+
+    # write the dictionary of products to an output .csv file
+    _write_file()
+    
+
+# Check if the script is run directly
+if __name__ == "__main__":
+    main()
---
+++ model.py
@@ -0,0 +1,84 @@
+import Target_Scraping as target
+import Aldi_Scraping as aldi
+import TJ_Scraping as tj
+
+import sys
+
+# call preloaded data by default
+target_dict = target.load_data()
+aldi_dict = aldi.load_data()
+tj_dict = tj.load_data()
+
+
+def refresh_data():
+    """
+    Asks each store scraping script to scrape fresh information from each website and updates the dictionaries.
+    """
+    global target_dict, aldi_dict, tj_dict
+
+    target_dict = target.scrape_data()
+    aldi_dict = aldi.scrape_data()
+    tj_dict = tj.scrape_data()
+
+
+def _search_store(prod_dict, keyword):
+    """
+    Finds and returns the cheapest product with the given keyword within the given dictionary of products.
+    """
+    min = (sys.float_info.max,)
+
+    # loops thru each dictionary item for each store
+    for product in prod_dict.items():
+        # format for product = (product name, (category, price, quantity))
+        if keyword.lower() in product[0].lower():
+            # strip the $ at the beginning of the price
+            price = float(product[1][1][1:])
+            if price < min[0]:
+                # min = (price as a float, (product name, category, price as a string, quantity))
+                min = (price, (product[0], product[1][0], product[1][1], product[1][2]))
+
+    # return the cheapest product as ((price as a float, (product name, category, price as a string, quantity)) for this store
+    return min
+
+
+def _find_cheapest(keyword):
+    """
+    Finds the cheapest product with the given keyword from the three stores.
+    """
+    # get the cheapest product for each store
+    target_min = _search_store(target_dict, keyword)
+    aldi_min = _search_store(aldi_dict, keyword)
+    tj_min = _search_store(tj_dict, keyword)
+
+    # find the cheapest product among the three stores
+    min = target_min
+    min_name = 'Target'
+
+    if aldi_min[0] < min[0]:
+        min = aldi_min
+        min_name = 'Aldi'
+
+    if tj_min[0] < min[0]:
+        min = tj_min
+        min_name = 'Trader Joes'
+
+    # return cheapest product as (store name, (product name, category, price as string, quantity))
+    return (min_name, min[1])
+
+#TODO: implement getting the distance to a store
+""" def find_distance(store_name):
+    return (dist, time) """
+
+
+def find_cheapest_product(keyword):
+    """
+    Orchestrates the process of finding the cheapest product with the matching keyword from the three stores and interfaces with the view
+    """
+    # get cheapest product in the format of (store name, (product name, category, price, quantity))
+    prod = _find_cheapest(keyword)
+    
+    #TODO: call the distance function with the store that offers the cheapest product
+    # distance = find_dist(prod[0])
+
+    #TODO: interface with the view - displaying the below information
+    # display, prod[0]<string name of store>, and prod[1]<tup = (product name, category, price, quantity)> on the view
